d3fe42d6e7b0f2112ec1c6ac11ec84b5d6293bab
armandnjapou <armand2ngassu@gmail.com>
2022-01-19 17:19:59 +0100

    US 3: Update Account class

--- src/main/java/business/Account.java
+++ src/main/java/business/Account.java
@@ -1,11 +1,15 @@
 package business;
 
+import infrastructure.IConsoleStatementPrinter;
 import infrastructure.exception.InsufficientBalanceException;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 public class Account {
     private Balance balance;
+    private Statement statement = new Statement();
+    private LocalDateTime localDateTime;
 
     public Account() {
         balance = new Balance(BigDecimal.ZERO);
@@ -15,12 +19,18 @@ public class Account {
         this.balance = balance;
     }
 
+    public Account(Balance balance, LocalDateTime localDateTime) {
+        this.balance = balance;
+        this.localDateTime = localDateTime;
+    }
+
     public Balance getBalance() {
         return balance;
     }
 
     public void deposit(Amount amount) {
         balance = balance.add(amount);
+        statement.add(new StatementLine(new Operation(OperationType.DEPOSIT, amount, LocalDateTime.now()), balance));
     }
 
     public void withdrawal(Amount amount) throws InsufficientBalanceException {
@@ -28,5 +38,10 @@ public class Account {
         if(BigDecimal.ZERO.compareTo(balance.value()) > 0){
             throw new InsufficientBalanceException("Insufficient balance for this operation");
         }
+        statement.add(new StatementLine(new Operation(OperationType.WITHDRAWAL, amount, LocalDateTime.now()), balance));
+    }
+
+    public void print(IConsoleStatementPrinter statementPrinter) {
+        statementPrinter.print(statement);
     }
 }
