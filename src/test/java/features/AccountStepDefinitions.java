c27663fff26d076cb5ae632c3086dc1d04ebcb1d
armandnjapou <armand2ngassu@gmail.com>
2022-01-20 11:17:03 +0100

    Create new scenario in withdrawal feature

--- src/test/java/features/AccountStepDefinitions.java
+++ src/test/java/features/AccountStepDefinitions.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 public class AccountStepDefinitions {
 
     private Account account;
+    Throwable throwable = null;
 
     @Given("account with balance equal to 1500")
     public void account_with_balance_equal_to_1500() {
@@ -39,5 +40,24 @@ public class AccountStepDefinitions {
     @Then("account balance equal to 200")
     public void account_balance_equal_to_200() {
         Assertions.assertEquals(new Balance(new BigDecimal(200)), account.getBalance());
+    }
+
+    @Given("account with balance of 300")
+    public void account_with_balance_of_300() {
+        account = new Account(new Balance(new BigDecimal(300)));
+    }
+
+    @When("withdrawal an amount of 400")
+    public void withdrawal_an_amount_of_400() {
+        try {
+            account.withdrawal(new Amount(new BigDecimal(400)));
+        } catch(Throwable throwable) {
+            this.throwable = throwable;
+        }
+    }
+
+    @Then("denied operation")
+    public void denied_operation() {
+        Assertions.assertTrue(throwable instanceof InsufficientBalanceException);
     }
 }
--- src/test/resources/features/withdrawal.feature
+++ src/test/resources/features/withdrawal.feature
@@ -1,7 +1,11 @@
 Feature: In order to retrieve some or all of my savings
   As a bank client
   I want to make a withdrawal from my account
-  Scenario: Withdrawal 500 on an account with balance equals to 700
+  Scenario: Withdrawal 500 from an account with balance equals to 700
     Given account with balance equal to 700
     When withdrawal an amount of 500
     Then account balance equal to 200
+  Scenario: Withdrawal 400 from an account with balanace of 300
+    Given account with balance of 300
+    When withdrawal an amount of 400
+    Then denied operation
